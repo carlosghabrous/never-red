9c51184aea8a919ef323437a0e945f02dcb53fb0
ghab <carlos.ghabrous@gmail.com>
2022-05-23 22:01:22 +0200

    DOCS: add swagger docs

--- server/server.go
+++ server/server.go
@@ -8,24 +8,42 @@ import (
 
 var mux *http.ServeMux
 
-// swagger:route GET /hello App says hello
+// swagger:route GET /hello Hello
 //
 // Makes the app say hello
 //
 // responses:
-//	200: OK
+// 		200
 func helloHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "hello there!\n")
 }
 
+// swagger:route POST /csv-import Import CSV file
+//
+// Imports data from a CSV file in a predefined format
+//
+// responses:
+// 		201: OK
 func csvImportHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "csv import!\n")
 }
 
+// swagger:route GET expenses Expenses
+//
+// Queries the DB to get expenses' data
+//
+// responses:
+// 		200: OK
 func expensesHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "expenses handler!\n")
 }
 
+// swagger:route GET incomes Incomes
+//
+// Queries the DB to get incomes' data
+//
+// responses:
+//		200: OK
 func incomesHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "incomes handler!\n")
 }
