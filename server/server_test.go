13cc54e39d97c0da3e80d4db07c4039abb620b1e
ghab <carlos.ghabrous@gmail.com>
2022-07-19 10:17:51 +0200

    TEST: write test functions' signatures

--- server/server_test.go
+++ server/server_test.go
@@ -2,9 +2,14 @@ package server
 
 import "testing"
 
-func TestHelloHandler(t *testing.T) {
-	got, want := 1, 1
-	if got != want {
-		t.Errorf("got %d, want %d", got, want)
-	}
+func TestStartServerHappyPath(t *testing.T) {
+
+}
+
+func TestStartServerReturnsErrorWhenEnvFileIsNotFound(t *testing.T) {
+
+}
+
+func TestStartServerReturnsErrorWhenPortIsNotFound(t *testing.T) {
+
 }
