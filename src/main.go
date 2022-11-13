c24cde2449dd3cb2951b644892e70ad129d2da85
ghab <carlos.ghabrous@gmail.com>
2022-11-13 10:39:27 +0100

    include multiple handlers

--- src/main.go
+++ src/main.go
@@ -6,10 +6,16 @@ import (
 	"net/http"
 )
 
-type MyHandler struct{}
+type HelloHandler struct{}
 
-func (handler *MyHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+func (handler *HelloHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "Hello!")
+}
+
+type WorldHandler struct{}
+
+func (handler *WorldHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "World!")
 }
 
 // type Config map[string]string
@@ -28,9 +34,13 @@ func (handler *MyHandler) ServeHTTP(writer http.ResponseWriter, request *http.Re
 // TODO: have the command line option of starting a TLS server?
 
 func main() {
-	soleHandler := MyHandler{}
 	serverAddr := "127.0.0.1" + ":" + "8080"
-	server := http.Server{Addr: serverAddr, Handler: &soleHandler}
+	server := http.Server{Addr: serverAddr, Handler: nil}
+
+	helloHandler := HelloHandler{}
+	worldHandler := WorldHandler{}
+	http.Handle("/hello", &helloHandler)
+	http.Handle("/world", &worldHandler)
 
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("Could not start server %e", err)
