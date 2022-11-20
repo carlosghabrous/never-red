231230ddb4799f16d5c18cae1aa777f9bbaf21f5
ghab <carlos.ghabrous@gmail.com>
2022-11-20 19:30:43 +0100

    include db pointer

--- src/pkg/server/api.go
+++ src/pkg/server/api.go
@@ -1,26 +1,44 @@
 package server
 
-import "net/http"
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+)
 
-type api struct {
-	router http.Handler
+type App struct {
+	router http.Handler // interface
+	db     *sql.DB
 }
 
-type Server interface {
-	Router() http.Handler
+var app App
+
+func New() (*App, error) {
+	app := &App{}
+	app.router = initAppRouter()
+
+	var err error
+	app.db, err = initDb()
+
+	if err != nil {
+		return nil, fmt.Errorf("Could not connect to the DB: %w", err)
+	}
+	return app, nil
 }
 
-func New() Server {
-	theApi := &api{}
-
+func initAppRouter() http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/import", theApi.dataImport)
-	mux.HandleFunc("/movements", theApi.getMovements)
+	mux.HandleFunc("/import", app.dataImport)
+	mux.HandleFunc("/movements", app.dataImport)
 
-	theApi.router = mux
-	return theApi
+	return mux
 }
 
-func (a *api) Router() http.Handler {
-	return a.router
+func initDb() (*sql.DB, error) {
+	//TODO: get DB credentials from somewhere
+	return nil, nil
+}
+
+func (app *App) Router() http.Handler {
+	return app.router
 }
