d42bf46247fdab37070d0bbebfa62d0662b565c5
ghab <carlos.ghabrous@gmail.com>
2022-12-03 23:26:18 +0100

    correct foreign key

--- postgres/0003_create_tables.sql
+++ postgres/0003_create_tables.sql
@@ -1,17 +1,3 @@
-CREATE TABLE IF NOT EXISTS movements (
-    id SERIAL PRIMARY KEY,
-    mov_date DATE NOT NULL,
-    mov_type VARCHAR(10), 
-    commerce VARCHAR(50),
-    category VARCHAR(50),
-    currency VARCHAR(3),
-    amount FLOAT, 
-
-    CONSTRAINT fk_movtype(mov_type) REFERENCES mov_type(id) ON DELETE SET NULL,
-    CONSTRAINT fk_category(category) REFERENCES category(id) ON DELETE SET NULL,
-    CONSTRAINT fk_currency(currency) REFERENCES currency(id) ON DELETE SET NULL
-);
-
 CREATE TABLE IF NOT EXISTS mov_type (
     id serial PRIMARY KEY,
     name VARCHAR(10)
@@ -25,3 +11,18 @@ CREATE TABLE IF NOT EXISTS currency (
     id serial PRIMARY KEY,
     name VARCHAR(3)
 );
+
+CREATE TABLE IF NOT EXISTS movements (
+    id SERIAL PRIMARY KEY,
+    mov_date DATE NOT NULL,
+    mov_type SMALLINT, 
+    commerce VARCHAR(50),
+    category SMALLINT,
+    currency SMALLINT,
+    amount REAL, 
+
+    CONSTRAINT fk_movtype FOREIGN KEY(mov_type) REFERENCES mov_type(id) ON DELETE SET NULL,
+    CONSTRAINT fk_category FOREIGN KEY (category) REFERENCES category(id) ON DELETE SET NULL,
+    CONSTRAINT fk_currency FOREIGN KEY (currency) REFERENCES currency(id) ON DELETE SET NULL
+);
+
